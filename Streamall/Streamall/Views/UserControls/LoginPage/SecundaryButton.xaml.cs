4942fbacfa07515d589ba859a3c9b2fb7500df4b
João Otávio <joaorosa0539@gmail.com>
2026-08-31 21:08:07 -0300

    Implementando tela de login, ainda não terminada

---
+++ Streamall/Streamall/Views/UserControls/LoginPage/SecundaryButton.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Streamall.MVVM;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Controls;
+
+namespace Streamall.Views.UserControls.LoginPage
+{
+    public partial class SecundaryButton : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _buttonContent;
+
+        public string ButtonContent
+        {
+            get { return _buttonContent; }
+            set 
+            { 
+                _buttonContent = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public SecundaryButton()
+        {
+            InitializeComponent();
+        }
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Streamall/Streamall/Views/UserControls/LoginPage/TextBoxController.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Streamall.Views.UserControls.LoginPage
+{
+    public partial class TextBoxController : UserControl, INotifyPropertyChanged
+    {
+        private string _placeholder;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string Placeholder
+        {
+            get { return _placeholder; }
+            set 
+            {
+                _placeholder = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public TextBoxController()
+        {
+            DataContext = this;
+            InitializeComponent();
+        }
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void txtInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtInput.Text))
+                tbText.Visibility = Visibility.Hidden;
+            else
+                tbText.Visibility = Visibility.Visible;
+        }
+    }
+}
