64f4e059ad00282ca1b6b2a76aada72daf24d572
João Otávio <joaorosa0539@gmail.com>
2026-09-07 21:10:01 -0300

    Iniciando montagem do design da tela de cadastro

--- Streamall/Streamall/Views/SignUp.xaml.cs
+++ Streamall/Streamall/Views/SignUp.xaml.cs
@@ -13,5 +13,37 @@ namespace Streamall.Views
             InitializeComponent();
             DataContext = new SignUpViewModel();
         }
+
+        private void txtFullName_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtFullName.Text))
+                tbFullName.Visibility = Visibility.Hidden;
+            else
+                tbFullName.Visibility = Visibility.Visible;
+        }
+
+        private void txtUserName_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtUserName.Text))
+                tbUserName.Visibility = Visibility.Hidden;
+            else
+                tbUserName.Visibility = Visibility.Visible;
+        }
+
+        private void txtEmail_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtEmail.Text))
+                tbEmail.Visibility = Visibility.Hidden;
+            else
+                tbEmail.Visibility = Visibility.Visible;
+        }
+
+        private void txtPassword_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtPassword.Text))
+                tbPassword.Visibility = Visibility.Hidden;
+            else
+                tbPassword.Visibility = Visibility.Visible;
+        }
     }
 }
