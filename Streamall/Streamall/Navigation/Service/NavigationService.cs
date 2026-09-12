bf37e0dd0ac255fb61f099314dd0710c17119705
João Otávio <joaorosa0539@gmail.com>
2026-09-12 16:39:00 -0300

    feat: Adicionando UserControls para controle de usuários e de conteúdos

--- Streamall/Streamall/Navigation/Service/NavigationService.cs
+++ Streamall/Streamall/Navigation/Service/NavigationService.cs
@@ -36,10 +36,10 @@ namespace Streamall.Navigation.Service
             if (typeof(TView) == typeof(AdministratorHome))
             {
                 Window loginView = Application.Current.Windows
-                    .OfType<Window>()
-                    .FirstOrDefault(w => w.IsActive);
-                AdministratorHome admHome = new AdministratorHome();
+                .OfType<Window>()
+                .FirstOrDefault(w => w.IsActive);
 
+                AdministratorHome admHome = new AdministratorHome();
                 admHome.WindowState = WindowState.Maximized;
                 admHome.Show();
                 loginView.Close();
